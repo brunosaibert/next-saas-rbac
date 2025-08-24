823045ea0910b2bc5587c7f80b2b2217cfe34bd7
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-24 13:17:06 -0300

    feat: prisma lib

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -7,7 +7,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
-import { createAccount } from './routes/create-account'
+import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
