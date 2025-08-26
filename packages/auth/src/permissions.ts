6abeb57b80180dd4a475ae4589b315846fc698ab
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-26 06:08:31 -0300

    feat: get membership

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,7 +2,7 @@ import type { AbilityBuilder } from '@casl/ability'
 
 import type { AppAbility } from '.'
 import type { User } from './models/user'
-import type { Role } from './role'
+import type { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
