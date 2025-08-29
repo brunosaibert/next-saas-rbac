152af00c0b1dfdcc73032ae023f1a52509f5eea2
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-28 21:25:10 -0300

    feat: init web app

--- config/eslint-config/next.cjs
+++ config/eslint-config/next.cjs
@@ -1,8 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@rocketseat/eslint-config/next'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', '@stylistic'],
   rules: {
     'simple-import-sort/imports': 'error',
+    '@stylistic/jsx-sort-props': 'error',
   },
 }
