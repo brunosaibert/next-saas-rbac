212221cddec5277d116ec631b250d8d45e17a298
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-29 09:13:49 -0300

    feat: auth pages

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import GithubIcon from '@/assets/github.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input id="email" name="email" type="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" name="password" type="password" />
+        <Link
+          className="text-foreground text-xs font-medium hover:underline"
+          href="/auth/forgot-password"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+      <Button className="w-full" type="submit">
+        Sign in with e-mail
+      </Button>
+      <Button asChild className="w-full" size="sm" variant="link">
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+      <Separator />
+      <Button className="w-full" type="submit" variant="outline">
+        <Image alt="" className="mr-2 size-4 dark:invert" src={GithubIcon} />
+        Sign in with Github
+      </Button>
+    </form>
+  )
+}
