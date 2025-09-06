d9dab2a72f5bb63d90d68319aae209964439c986
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-09-06 20:07:40 -0300

    feat: interception routes

---
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import OrganizationForm from '@/app/(app)/create-organization/organization-form'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet'
+
+export default function CreateOrganization() {
+  const router = useRouter()
+
+  return (
+    <Sheet
+      defaultOpen
+      onOpenChange={() => {
+        router.back()
+      }}
+    >
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Create organization</SheetTitle>
+        </SheetHeader>
+        <div className="px-4">
+          <OrganizationForm />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,7 +1,12 @@
+import { Header } from '@/components/header'
+
 export default async function Home() {
   return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-bold">Home</h1>
+    <div className="space-y-4 p-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p className="text-muted-foreground text-sm">Select an organization.</p>
+      </main>
     </div>
   )
 }
